bba0429284b47f452d82aa54286bd3b958fd5269
Kasun <kasunkavinda.bc@gmail.com>
2022-03-08 23:21:14 +0530

    latest ui changes

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,21 +1,26 @@
-import classes from './Header.module.css'
+import classes from "./Header.module.css";
 
 function Header() {
   return (
-    <div className="grid grid-flow-col grid-cols-1 grid-rows-3 gap-4">
-      <div className = {classes.triangle}>
-
+    // <div className="grid grid-flow-col grid-cols-1 grid-rows-3 gap-4">
+    <div className="">
+      <div className={classes.triangle}></div>
+      <div className={classes.header1}>
+        <h1 className="leading-none">Hi There!</h1>
       </div>
-      <div className = {classes.header1}>
-        <h1>Hi There!</h1>
+      <div className={classes.header2}>
+        <h2 className="leading-none">I am Kasun Kavinda</h2>
       </div>
-      <div className = {classes.header2}>
-        <h2>I am Kasun Kavinda</h2>
-      </div>
-      <div className="text-center">
+      <div className={`text-center sm:mt-32 ${classes.tagline}`}>
         <p>
-          UI/ UX Engineer, Front End Developer, based in SriLanka. <br/>I like
-          building elegant websites and bringing <br/>creative designs to life.{" "}
+          I’m living the dream. <br />
+          <br />I started learning to code when I was a teenager, though it was
+          always more of a hobby than a career focus.
+          <br />
+          I’m familiar with a variety of programming languages and frameworks
+          including <br />
+          JavaScript, Angular, React, Next, NodeJs <br />
+          but I’m always adding new skills to my repertoire. <br />
         </p>
       </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
+import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout/Layout";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <Layout>
       <Component {...pageProps} />
