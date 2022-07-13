95de9b85131a44670358abd3d6d96a83fa60c6c9
kasunkavinda <kasunkavinda.bc@gmail.com>
2022-07-13 10:44:30 +0530

    skills updated/ contact page issue fixed

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,16 +1,18 @@
 //POST api/contact-me
 import { MongoClient } from "mongodb";
 
-const database_url = process.env.DATABBASE_URL;
+const database_url = process.env.DATABASE_URL;
 
 async function handler(req, res) {
+  console.log('contactCollection', req.method);
   if (req.method === "POST") {
     const data = req.body;
-
+    
     const client = await MongoClient.connect(database_url);
-
+    console.log('data', data)
     const db = client.db();
     const contactCollection = db.collection("ContactMe");
+    
     const result = await contactCollection.insertOne(data);
     client.close();
     res.status(201).json({ message: "Contact Record Inserted" });
