1a83dc3aa8646abaae5fd756fb3959abde673ffa
kasunkavinda <kasunkavinda.bc@gmail.com>
2023-03-25 23:26:00 +0530

    transition update

--- components/skillbox/Skillbox.js
+++ components/skillbox/Skillbox.js
@@ -14,7 +14,7 @@ const StyledProgress = styled(ProgressPrimitive.Root, {
 const StyledIndicator = styled(ProgressPrimitive.Indicator, {
   backgroundColor: "#3360FF",
   height: "100%",
-  transition: "width 660ms cubic-bezier(0.65, 0, 0.35, 1)",
+  // transition: "width 660ms cubic-bezier(0.65, 0, 0.35, 1)",
 });
 
 // Exports
@@ -22,17 +22,17 @@ export const Progress = StyledProgress;
 export const ProgressIndicator = StyledIndicator;
 function Skillbox() {
   const [progress1, setProgress1] = React.useState(13);
-  React.useEffect(() => {setTimeout(() => setProgress1(80), 500), []});
+  React.useEffect(() => { setTimeout(() => setProgress1(80), 500), [] });
   const [progress2, setProgress2] = React.useState(13);
-  React.useEffect(() => {setTimeout(() => setProgress2(80), 500), []});
+  React.useEffect(() => { setTimeout(() => setProgress2(80), 500), [] });
   const [progress3, setProgress3] = React.useState(13);
-  React.useEffect(() => {setTimeout(() => setProgress3(70), 500), []});
+  React.useEffect(() => { setTimeout(() => setProgress3(70), 500), [] });
   const [progress4, setProgress4] = React.useState(13);
-  React.useEffect(() => {setTimeout(() => setProgress4(90), 500), []});
+  React.useEffect(() => { setTimeout(() => setProgress4(90), 500), [] });
   const [progress5, setProgress5] = React.useState(13);
-  React.useEffect(() => {setTimeout(() => setProgress5(50), 500), []});
+  React.useEffect(() => { setTimeout(() => setProgress5(50), 500), [] });
   const [progress6, setProgress6] = React.useState(13);
-  React.useEffect(() => {setTimeout(() => setProgress6(80), 500), []});
+  React.useEffect(() => { setTimeout(() => setProgress6(80), 500), [] });
   return (
     <div>
       <h1 className={classses.skillboxheader}>Skills</h1>
@@ -93,7 +93,7 @@ function Skillbox() {
           </div>
         </div>
       </div>
-      
+
     </div>
   );
 }
